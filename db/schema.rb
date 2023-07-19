76a531ab653fba15991c2c8afa5ee72d5ecc92f5
nbjun1 <ec2-user@ip-172-31-24-145.ap-northeast-1.compute.internal>
2023-07-19 14:08:20 +0000

    C11 end

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_19_062304) do
+ActiveRecord::Schema.define(version: 2023_07_19_135323) do
 
   create_table "post_images", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "shop_name"
+    t.text "caption"
   end
 
   create_table "postimages", force: :cascade do |t|
