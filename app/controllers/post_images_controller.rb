934c6a7ef2de9bd0691dada42451c0d5fff615d2
nbjun1 <ec2-user@ip-172-31-24-145.ap-northeast-1.compute.internal>
2023-07-21 06:33:54 +0000

    C15 end

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,25 +1,29 @@
 class PostImagesController < ApplicationController
 
   def new
+
     @post_image = PostImage.new
   end
 
   #　投稿データの保存
   def create
-    #byebug
+
     @post_image = PostImage.new(post_image_params)
+
     @post_image.user_id = current_user.id
     @post_image.save
     redirect_to post_images_path
   end
 
   def index
-    @post_image = PostImage.all
+    @post_images = PostImage.all
     @post_image = PostImage.new
   end
 
   def show
+    #byebug
     @post_image = PostImage.where(id: params[:id])
+    @post_image = post_image.id
   end
 
   # 投稿データのストロングパラメータ
--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -4,8 +4,8 @@ class PostImage < ApplicationRecord
   belongs_to :user
 
   def get_image
-    unless image.atteched?
-      file_path = Rails.root.join('app/assets/images/np_image.jpg')
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
 
     end
