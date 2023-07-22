218f9bdd3dd785998a5897c84e4f2cad33276c8d
nbjun1 <ec2-user@ip-172-31-24-145.ap-northeast-1.compute.internal>
2023-07-22 07:34:44 +0000

    app2 end

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,13 +5,16 @@ class UsersController < ApplicationController
   end
 
   def edit
+
     @user = User.find(params[:id])
+
   end
 
   def update
+
     @user = User.find(params[:id])
-    @user.update
-    redirect_to show_user_path
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
   end
 
 
@@ -19,5 +22,12 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :profile_image)
+  end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end
   end
 end
